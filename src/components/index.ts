d14dbad00494dad85017d49c0345438247ab76f3
Habeeb <habeebahmadu1@gmail.com>
2024-05-28 15:59:37 +0100

    feat: add user settings component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 import { Header } from "@/components/header";
 import { Logo } from "@/components/logo";
+import { UserSettings } from "@/components/user-settings";
 
-export { Header, Logo };
+export { Header, Logo, UserSettings };
---
+++ src/components/user-settings.tsx
@@ -0,0 +1,35 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import Link from "next/link";
+import { User } from "lucide-react";
+
+export const UserSettings = () => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <div className="w-8 h-8 bg-gray-200 rounded-full flex items-center group justify-center cursor-pointer hover:bg-gray-300 transition-all">
+          <User className="w-4 h-4 font-bold group-hover:text-btn " />
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem>
+          <Link href="/login">Login</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem>
+          <Link href="/register">Register</Link>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
